0edf6f3f618f8b072a5b6948118a5a216b2ff4da
louis2263 <louisfong@gmail.com>
2026-08-12 16:37:52 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,41 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const { Server } = require('socket.io');
 
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+  }
+});
+
+// 提供 public 資料夾靜態檔案
+app.use(express.static(path.join(__dirname, 'public')));
+
+// 根路由手動發送 index.html
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+io.on('connection', (socket) => {
+  console.log('User connected:', socket.id);
+
+  socket.on('location-update', (data) => {
+    socket.broadcast.emit('user-location', {
+      id: socket.id,
+      ...data
+    });
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected:', socket.id);
+    io.emit('user-disconnected', socket.id);
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
+});
