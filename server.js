dbda3a08aa6152be37569c6a27b12d4fa15a99b8
louis2263 <louisfong@gmail.com>
2026-08-12 15:53:02 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const http = require('http');
 const path = require('path');
+const { Server } = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+  }
+});
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -11,11 +17,23 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-module.exports = app;
+io.on('connection', (socket) => {
+  console.log('User connected:', socket.id);
 
-if (require.main === module) {
-  const PORT = process.env.PORT || 3000;
-  server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  socket.on('location-update', (data) => {
+    socket.broadcast.emit('user-location', {
+      id: socket.id,
+      ...data
+    });
   });
-}
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected:', socket.id);
+    io.emit('user-disconnected', socket.id);
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
